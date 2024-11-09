16dda20284d7308673cbf307bb4c0b8b97fe160a
Jay <jayolesonbz@gmail.com>
2024-11-09 13:28:16 -0600

    um ok

--- scripts/utility.py
+++ scripts/utility.py
@@ -254,12 +254,12 @@ def get_current_season():
     modifiers = {"Newleaf": 0, "Greenleaf": 3, "Leaf-fall": 6, "Leaf-bare": 9}
     index = game.clan.age % 12 + modifiers[game.clan.starting_season]
 
-        if index > 11:
-            index = index - 12
+    if index > 11:
+        index = index - 12
 
-        game.clan.current_season = game.clan.seasons[index]
+    game.clan.current_season = game.clan.seasons[index]
 
-        return game.clan.current_season
+    return game.clan.current_season
 
 
 def change_clan_reputation(difference):
